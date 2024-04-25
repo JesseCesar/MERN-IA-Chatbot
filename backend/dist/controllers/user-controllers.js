7441803062ce07b0a74e7ea67be650e2c4aadcfd
Cesar Alexandre <cesaralexandreprofissional@gmail.com>
2024-04-25 20:41:39 +0100

    set-up

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -7,6 +8,19 @@ export const getAllUsers = async (req, res, next) => {
     catch (error) {
         console.error("Error in getAllUsers:", error);
         res.status(500).json({ message: "Server error", error });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(200).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ message: "Error creating user", error });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -12,5 +13,22 @@ export const getAllUsers = async (
   } catch (error) {
     console.error("Error in getAllUsers:", error);
     res.status(500).json({ message: "Server error", error });
+  }
+}
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(200).json({ message: "OK", id: user._id.toString()  });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Error creating user", error });
   }
 }
